66acf42794ed69e4475b025f9ed4355eb4b1ce4e
sourav chakraborty <sourav@turbot.com>
2022-09-15 10:16:04 +0800

    Add default error handling

--- uptimerobot/plugin.go
+++ uptimerobot/plugin.go
@@ -11,12 +11,19 @@ import (
 func Plugin(ctx context.Context) *plugin.Plugin {
 	p := &plugin.Plugin{
 		Name:             "steampipe-plugin-uptimerobot",
-		DefaultTransform: transform.FromGo(),
+		DefaultTransform: transform.FromCamel(),
+		DefaultGetConfig: &plugin.GetConfig{
+			IgnoreConfig: &plugin.IgnoreConfig{
+				ShouldIgnoreErrorFunc: isNotFoundError([]string{"not found"}),
+			},
+		},
 		ConnectionConfigSchema: &plugin.ConnectionConfigSchema{
 			NewInstance: ConfigInstance,
 			Schema:      ConfigSchema,
 		},
-		TableMap: map[string]*plugin.Table{},
+		TableMap: map[string]*plugin.Table{
+			"uptimerobot_monitor": tableUptimeRobotMonitor(ctx),
+		},
 	}
 
 	return p
