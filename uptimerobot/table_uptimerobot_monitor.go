d2d3a52800ddff9034aee56b1d962a13f61622f1
Priyanka-Chatterjee-2000 <priyankachatterjee9075@gmail.com>
2022-09-16 17:39:08 +0800

    KeyColumns added

--- uptimerobot/table_uptimerobot_monitor.go
+++ uptimerobot/table_uptimerobot_monitor.go
@@ -16,32 +16,124 @@ func tableUptimerobotMonitor(ctx context.Context) *plugin.Table {
 		Description: "UptimeRobot Retrieve Websites Monitored Current Account.",
 		List: &plugin.ListConfig{
 			Hydrate: listMonitors,
+			KeyColumns: []*plugin.KeyColumn{
+				{
+					Name:    "type",
+					Require: plugin.Optional,
+				},
+				{
+					Name:    "status",
+					Require: plugin.Optional,
+				},
+			},
 		},
 		Get: &plugin.GetConfig{
 			KeyColumns: plugin.SingleColumn("id"),
 			Hydrate:    getMonitor,
 		},
+		// All columns
 		Columns: []*plugin.Column{
-			{Name: "id", Type: proto.ColumnType_STRING, Description: "The unique account id for monitors."},
-			{Name: "friendly_name", Type: proto.ColumnType_STRING, Description: "The friendly name for monitors."},
-			{Name: "url", Type: proto.ColumnType_STRING, Description: "The url/ip of the monitored website."},
-			{Name: "type", Type: proto.ColumnType_INT, Description: "Type of the website monitored."},
-			{Name: "sub_type", Type: proto.ColumnType_STRING, Description: "Subtype of the website monitored."},
-			{Name: "keyword_type", Type: proto.ColumnType_STRING, Description: "The keyword of the monitors."},
-			{Name: "keyword_case_type", Type: proto.ColumnType_INT, Description: "The keyword case of the monitors."},
-			{Name: "port", Type: proto.ColumnType_STRING, Description: "The port of the website monitored."},
-			{Name: "keyword_value", Type: proto.ColumnType_STRING, Description: "The keyword value of the website."},
-			{Name: "http_username", Type: proto.ColumnType_STRING, Description: "The http-username for password-protected web pages."},
-			{Name: "http_password", Type: proto.ColumnType_STRING, Description: "The http-password for password-protected web pages."},
-			{Name: "interval", Type: proto.ColumnType_INT, Description: "The interval for the monitoring check ."},
-			{Name: "timeout", Type: proto.ColumnType_INT, Description: "The timeout for the monitoring check ."},
-			{Name: "create_datetime", Type: proto.ColumnType_TIMESTAMP, Description: "The creation time for the monitor.", Transform: transform.FromField("CreateDatetime.Time")},
-			{Name: "monitor_group", Type: proto.ColumnType_INT, Description: "The groups for monitors."},
-			{Name: "is_group_main", Type: proto.ColumnType_INT, Description: "The main groups for monitors."},
-			{Name: "alert_contacts", Type: proto.ColumnType_JSON, Description: "The alert contacts of the monitors."},
-			{Name: "logs", Type: proto.ColumnType_JSON, Description: "The logs of the monitors."},
-			{Name: "status", Type: proto.ColumnType_INT, Description: "The status of the monitored website."},
-			{Name: "ssl", Type: proto.ColumnType_JSON, Description: "The ssl of the monitors."},
+			{
+				Name:        "id",
+				Type:        proto.ColumnType_STRING,
+				Description: "The unique account id for monitors.",
+			},
+			{
+				Name:        "friendly_name",
+				Type:        proto.ColumnType_STRING,
+				Description: "The friendly name for monitors.",
+			},
+			{
+				Name:        "url",
+				Type:        proto.ColumnType_STRING,
+				Description: "The url/ip of the monitored website.",
+			},
+			{
+				Name: "type",
+				Type:        proto.ColumnType_INT,
+				Description: "Type of the website monitored.",
+			},
+			{
+				Name: "sub_type",
+				Type: proto.ColumnType_STRING, 
+				Description: "Subtype of the website monitored.",
+			},
+			{
+				Name: "keyword_type",
+				Type: proto.ColumnType_STRING,
+				Description: "The keyword of the monitors.",
+			},
+			{
+				Name: "keyword_case_type", 
+				Type: proto.ColumnType_INT, 
+				Description: "The keyword case of the monitors.",
+			},
+			{
+				Name: "port", 
+				Type: proto.ColumnType_STRING, 
+				Description: "The port of the website monitored.",
+			},
+			{
+				Name: "keyword_value", 
+				Type: proto.ColumnType_STRING, 
+				Description: "The keyword value of the website.",
+			},
+			{
+				Name: "http_username", 
+				Type: proto.ColumnType_STRING, 
+				Description: "The http-username for password-protected web pages.",
+			},
+			{
+				Name: "http_password", 
+				Type: proto.ColumnType_STRING, 
+				Description: "The http-password for password-protected web pages.",
+			},
+			{
+				Name: "interval", 
+				Type: proto.ColumnType_INT, 
+				Description: "The interval for the monitoring check .",
+			},
+			{
+				Name: "timeout", 
+				Type: proto.ColumnType_INT, 
+				Description: "The timeout for the monitoring check .",
+			},
+			{
+				Name: "create_datetime", 
+				Type: proto.ColumnType_TIMESTAMP, 
+				Description: "The creation time for the monitor.", 
+				Transform: transform.FromField("CreateDatetime.Time"),
+			},
+			{
+				Name: "monitor_group",
+				Type: proto.ColumnType_INT, 
+				Description: "The groups for monitors.",
+			},
+			{
+				Name: "is_group_main", 
+				Type: proto.ColumnType_INT, 
+				Description: "The main groups for monitors.",
+			},
+			{
+				Name: "alert_contacts",
+				Type: proto.ColumnType_JSON, 
+				Description: "The alert contacts of the monitors.",
+			},
+			{
+				Name: "logs", 
+				Type: proto.ColumnType_JSON, 
+				Description: "The logs of the monitors.",
+			},
+			{
+				Name: "status", 
+				Type: proto.ColumnType_INT, 
+				Description: "The status of the monitored website.",
+			},
+			{
+			Name: "ssl", 
+			Type: proto.ColumnType_JSON, 
+			Description: "The ssl of the monitors.",
+		},
 		},
 	}
 }
@@ -52,8 +144,22 @@ func listMonitors(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateDat
 		plugin.Logger(ctx).Error("listMonitor", "connection_error", err)
 		return nil, err
 	}
-
-	input := uptimerobotapi.GetMonitorsParams{Limit: types.Int(50)}
+	input := uptimerobotapi.GetMonitorsParams{
+		//sets default limit
+		Limit: types.Int(50),
+	}
+	typeMonitor := d.KeyColumnQuals["type"].GetStringValue()
+	status := d.KeyColumnQuals["status"].GetStringValue()
+	if(typeMonitor != ""){
+	input = uptimerobotapi.GetMonitorsParams{
+		Types: &typeMonitor,
+	}
+}
+	if(status != ""){
+		input = uptimerobotapi.GetMonitorsParams{
+			Statuses:&status,
+		}
+	}
 	limit := d.QueryContext.Limit
 	if d.QueryContext.Limit != nil {
 		if *limit < int64(*input.Limit) {
@@ -64,7 +170,6 @@ func listMonitors(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateDat
 			}
 		}
 	}
-
 	monitors, err := conn.Monitor.GetMonitors(input)
 	if err != nil {
 		plugin.Logger(ctx).Error("listMonitor", "api_error", err)
