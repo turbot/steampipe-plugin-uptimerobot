911de322b5585c24ee7c5fc63ea6ea2f4781eb02
Niharika Dutta <64894768+niharika2k00@users.noreply.github.com>
2022-09-16 15:59:54 +0530

    Add table uptimerobot_alert_contact. (#5)

---
+++ uptimerobot/table_uptimerobot_alert_contact.go
@@ -0,0 +1,120 @@
+package uptimerobot
+
+import (
+	"context"
+
+	"github.com/bigdatasourav/uptimerobotapi"
+	"github.com/turbot/go-kit/types"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
+)
+
+//// TABLE DEFINITION
+func tableUptimeRobotAlertContact(ctx context.Context) *plugin.Table {
+	return &plugin.Table{
+		Name:        "uptimerobot_alert_contact",
+		Description: "UptimeRobot Alert Contact",
+		List: &plugin.ListConfig{
+			Hydrate: listAlertContacts,
+		},
+		Get: &plugin.GetConfig{
+			KeyColumns: plugin.SingleColumn("id"),
+			Hydrate:    getAlertContact,
+		},
+		Columns: []*plugin.Column{
+			{
+				Name:        "id",
+				Type:        proto.ColumnType_STRING,
+				Description: "Unique id of the alert contact.",
+			},
+			{
+				Name:        "friendly_name",
+				Type:        proto.ColumnType_STRING,
+				Description: "Friendly name of the alert contact.",
+			},
+			{
+				Name:        "status",
+				Type:        proto.ColumnType_INT,
+				Description: "Status of the alert contact.",
+			},
+			{
+				Name:        "type",
+				Type:        proto.ColumnType_INT,
+				Description: "Type of the alert contact.",
+			},
+			{
+				Name:        "value",
+				Type:        proto.ColumnType_STRING,
+				Description: "Value of the alert contact.",
+			},
+		},
+	}
+}
+
+//// LIST FUNCTION
+func listAlertContacts(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
+	conn, err := connect(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("listAlertContacts", "connection_error", err)
+		return nil, err
+	}
+
+	maxVal := types.Int(50)
+
+	// If the requested number of items is less than the paging max limit set the limit to that instead
+	val1 := d.QueryContext.Limit
+	if val1 != nil {
+		if *val1 < 1 {
+			maxVal = types.Int(1)
+		}
+		if *val1 < int64(*maxVal) {
+			maxVal = types.Int(int(*val1))
+		}
+	}
+
+	var params = uptimerobotapi.GetAlertContactsParams{Limit: maxVal}
+
+	contacts, err := conn.AlertContact.GetAlertContacts(params)
+	if err != nil {
+		plugin.Logger(ctx).Error("listAlertContact", "api_error", err)
+		return nil, err
+	}
+
+	for _, item := range contacts.AlertContacts {
+		d.StreamListItem(ctx, item)
+	}
+
+	return nil, nil
+}
+
+//// HYDRATE FUNCTIONS
+func getAlertContact(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	conn, err := connect(ctx, d)
+	if err != nil {
+		plugin.Logger(ctx).Error("getAlertContact", "connection_error", err)
+		return nil, err
+	}
+
+	id := d.KeyColumnQuals["id"].GetStringValue()
+
+	// Check if id is empty.
+	if id == "" {
+		return nil, nil
+	}
+
+	params := uptimerobotapi.GetAlertContactsParams{
+		AlertContacts: &id,
+	}
+	result, err := conn.AlertContact.GetAlertContacts(params)
+
+	if err != nil {
+		plugin.Logger(ctx).Error("getAlertContact", "query_error", err)
+		return nil, err
+	}
+
+	if len(result.AlertContacts) > 0 {
+		return result.AlertContacts[0], nil
+	}
+
+	return nil, nil
+}
